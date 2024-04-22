8b34307f6b2fff49c79f145d246b61677fd9f2b5
Hikapasya05 <hikapasya05@gmail.com>
2024-04-22 12:17:23 +0700

    feat(converter): added date to readable string converter

--- common/src/main/java/com/hika/common/common/Converter.kt
+++ common/src/main/java/com/hika/common/common/Converter.kt
@@ -1,5 +1,9 @@
 package com.hika.common.common
 
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 fun Int.toRupiahFormat(): String {
     val rupiah = StringBuilder(toString())
     var i = rupiah.length - 3
@@ -8,4 +12,14 @@ fun Int.toRupiahFormat(): String {
         i -= 3
     }
     return "Rp$rupiah"
+}
+
+fun Date.toOrderHistoryFormat(): String {
+    val date = this.toString()
+    val inputFormat = SimpleDateFormat("EEE MMM dd HH:mm:ss 'GMT'Z yyyy", Locale.ENGLISH)
+    val outputFormat = SimpleDateFormat("MMM dd yyyy HH:mm:ss", Locale.ENGLISH)
+
+    val result = inputFormat.parse(date) ?: this
+
+    return outputFormat.format(result)
 }
