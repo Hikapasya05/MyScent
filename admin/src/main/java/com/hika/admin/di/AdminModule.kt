d653372bc12e5e133bfb6b48b965da4255f0f774
Hikapasya05 <hikapasya05@gmail.com>
2024-04-29 13:02:17 +0700

    fix(dependency): injected AdminProfileViewModel

--- admin/src/main/java/com/hika/admin/di/AdminModule.kt
+++ admin/src/main/java/com/hika/admin/di/AdminModule.kt
@@ -3,6 +3,7 @@ package com.hika.admin.di
 import com.hika.admin.features.home.HomeAdminViewModel
 import com.hika.admin.features.order.OrderAdminViewModel
 import com.hika.admin.features.product.ProductManagerViewModel
+import com.hika.admin.features.profile.ProfileAdminViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -10,4 +11,5 @@ val adminModule = module {
     viewModel { HomeAdminViewModel(get()) }
     viewModel { OrderAdminViewModel(get()) }
     viewModel { ProductManagerViewModel(get()) }
+    viewModel { ProfileAdminViewModel(get()) }
 }
